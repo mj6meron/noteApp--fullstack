80288528cbd1d450e433e0762ccf4656c5de4a30
mj6meron <meronmkl@gmail.com>
2022-04-11 19:07:35 +0200

    partially done

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,25 @@
 'use strict';
-console.log('----- started the app -------')
-
-const fs = require('fs');
-
-let rawdata = fs.readFileSync('./users.json');
-let usersData = JSON.parse(rawdata);
-
-
-
-
-
-console.log(usersData)
-console.log("whatsup lil nigga!")
-
-
-
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const fs = require('fs');
+const port = process.env.PORT || 1337; 
 
-var obj = require('./users.json').users;
-
+//  -------  get the usersData
+let rawdata = fs.readFileSync('./users.json');
+let usersData = JSON.parse(rawdata);
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json());
-
+app.use(express.static('frontend'))
 
 // Post request to create a new user
-app.post('/CreateUser', (req, res) => {
-
-  console.log("---------------------")
+app.post('/CreateNote', (req, res) => {
   console.log("post request to the server --> ", req.body)
-
   usersData.users.push({"note": `${req.body.note}`, "date": `${req.body.date}`, "done": `${false}`})
-
   const jsonString = JSON.stringify(usersData);
-
+  res.send('good job! we added a new note!')
   fs.writeFile('./users.json', jsonString, err => {
     if (err) {
       console.log('Error writing file', err);
@@ -44,26 +27,12 @@ app.post('/CreateUser', (req, res) => {
       console.log('Successfully wrote file');
     }
   });
-  console.log("---------------------")
-//  obj.newThing = req.body;
-//  fs.appendFile('./users.json', JSON.stringify(obj), function (err) {
-//  console.log(err);
-//});
 })
 
-// Get request to fetch the data
-app.get("/api", (req, res) => {
-    res.json(usersData)
-    console.log("get request to the server!")
-})
-app.get("/", (req, res) => {
-  res.json("Welcome to this page!")
-  console.log("get request to the server!")
+// Routes - REST APIs
+app.get('/getUsers', (req, res) => {
+  res.send(usersData)
 })
 
-app.post('/api/posts', (req, res) => {
-  res.send(req.body.users.map(x=>x))
-})
 
-const port = process.env.PORT || 3080; 
 app.listen(port, ()=> console.log(`Listning on port port - ${port}`));
